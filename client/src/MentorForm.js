b677b2bd7baddf6495bb0cdf1da3ad3fa86b981f
simvirk5 <simvirk5@gmail.com>
2018-05-12 18:49:01 -0700

    experience field saves in database

--- client/src/MentorForm.js
+++ client/src/MentorForm.js
@@ -3,6 +3,12 @@ import axios from 'axios';
 import Select from 'react-select';
 import 'react-select/dist/react-select.css';
 import Search from './Search';
+import { Collapsible, CollapsibleItem, Input } from 'react-materialize'
+
+const medicine = ['Dentistry', 'Gyno', 'Pediatrics']
+// const Arts = ['Photography', 'Animation', 'Sculpture', 'Illustration', 'Theatre', 'Strathmore watercolor paper', "Moleskine watercolor journal"]
+// const Law = ['Attorney', 'Prosecutor', 'Public Defender', 'Private Practice']
+// const Tech = ['Extra-smooth Bristol board', 'Strathmore mixed media paper', 'Micron technical pens']
 
 
 class MentorForm extends Component {
@@ -14,21 +20,72 @@ class MentorForm extends Component {
 		};
 	}
 	
-	fieldChange = (field) => {
-		this.setState({ field });
+	fieldChange = (e) => {
+		var newField = []
+		newField.push(e.target.value)
+		this.setState({field: newField});
+
 	}
 	
 	experienceChange = (e) => {
+		// console.log(e.target.value)
 		this.setState({ experience: e.target.value });
+	}
+
+	formSubmit = (e) => {
+		e.preventDefault();
+		console.log("Mentor form created!", this.state);
+		console.log('user is', this.props.user);
+		// this.state.field.push(this.state.field);
+		axios.post('http://localhost:3001/mentor', {
+			userId: this.props.user.id, 
+			// field: ['something here'],
+			field: this.state.field,
+			experience: this.state.experience
+		})
+		.then(result => {
+			this.setState({ 
+				field: result.data.field,
+				experience: result.data.experience
+			});
+			console.log(result)
+			console.log('Yay, it worked!', result);
+		})
+		.catch(err => {
+			console.log('ERROR', err);
+		});
+		
 	}
 
 render() {
 
+ var medicineMap = medicine.map((item,index) =>
+     <div>
+       <li key={index} className='fieldList'>
+        <Input name='field' type='checkbox' value= {item} label= {item} className='filled-in style-checkbox' onChange={this.fieldChange} />
+       </li>
+       <br />
+      </div>)
+
+
+
+
 		return (
 			<div>
-				<form onSubmit = {this.props.formSubmit}>	
+				<form onSubmit = {this.formSubmit}>	
 					<div>
-						<input name = "Experience" placeholder= "Enter short bio here" value = {this.state.experience} onChange = {this.experienceChange} />
+						<Collapsible accordion name="Field" >
+						  <CollapsibleItem header='Medicine' icon='arrow_drop_down_circle'>
+						  	<ul>{medicineMap}</ul>
+						  </CollapsibleItem>
+						  <CollapsibleItem header="Gyno" icon='arrow_drop_down_circle'>
+						  <ul><li>Gyno</li></ul>
+						  </CollapsibleItem>
+						  <CollapsibleItem header="Pediatrics" icon='arrow_drop_down_circle'>
+						   <ul><li>Pediatrics</li></ul>
+						  </CollapsibleItem>
+						</Collapsible>
+						<input name = "Experience" placeholder= "Enter short bio here" value = {this.state.experience} onChange = { this.experienceChange } />
 					</div>
 					<input type = "submit" value = "Submit" className = "button" />
 				</form>
--- client/src/Profile.js
+++ client/src/Profile.js
@@ -48,41 +48,18 @@ if (this.props.user) {
 	}
 }
 //passing formsubmit as props in mentorform
-	formSubmit = (e) => {
-		e.preventDefault();
-		console.log("Mentor form created!", this.state);
-		console.log('user is', this.props.user);
-		axios.post('http://localhost:3001/mentor', {
-			userId: this.props.user.id, 
-			field: ['something here'],
-			// field: this.props.field,
-			experience: this.props.experience
-		})
-		.then(result => {
-			this.setState({ 
-				field: result.data.field 
-			});
-			console.log(result)
-			console.log('Yay, it worked!', result);
-		})
-		.catch(err => {
-			console.log('ERROR', err);
-		});
-	}
 
-  
-
-
-
-	
 	render() {
 		if(this.props.user && this.props.user.mentor){
 			return (
 				<div>
 					<h1>Hello again, {this.props.user.name}!</h1>
 					<h3>Your email is {this.props.user.email}</h3>
-					<MentorForm user={this.props.user} formSubmit={this.formSubmit} />
-					<p>{this.state.field}</p>
+					<MentorForm user={this.props.user}/>
+					<p>This is state field {this.state.field}</p>
+					<p>This is state experience{this.state.experience}</p>
+					<p>This is props field{this.props.user.field}</p>
+					<p>This is the props experience{this.props.user.experience}</p>
 				</div>
 			);
 		} else if (this.props.user && !this.props.user.mentor) {
